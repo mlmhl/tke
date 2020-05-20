mlmhl/tke

commit eed3fda4518df0d3be481669f22ca76ae83833b9

feat(console): alarm support ns ALL and Workload All

web/console/helpers/urlUtil.ts
@@ -116,20 +116,22 @@ interface K8sRestfulPathOptions {
  * @param clusterId: string 集群id，适用于addon 请求平台转发的场景
  */
 export const reduceK8sRestfulPath = (options: K8sRestfulPathOptions) => {
-  let { resourceInfo, namespace = '', specificName = '', extraResource = '', clusterId = '', meshId } = options;
+  let {
+    resourceInfo,
+    namespace = '',
+    middleKey = '',
+    specificName = '',
+    extraResource = '',
+    clusterId = '',
+    meshId
+  } = options;
 
   /// #if project
   //业务侧ns eg: cls-xxx-ns 需要去除前缀
   if (namespace) {
     namespace = namespace.startsWith('global')
-      ? namespace
-          .split('-')
-          .splice(1)
-          .join('-')
-      : namespace
-          .split('-')
-          .splice(2)
-          .join('-');
+      ? namespace.split('-').splice(1).join('-')
+      : namespace.split('-').splice(2).join('-');
   }
   /// #endif
   let url: string = '';
@@ -174,15 +176,18 @@ export function reduceNs(namesapce) {
   /// #if project
   //业务侧ns eg: cls-xxx-ns 需要去除前缀
   if (newNs) {
-    newNs = newNs.startsWith('global')
-      ? newNs
-          .split('-')
-          .splice(1)
-          .join('-')
-      : newNs
-          .split('-')
-          .splice(2)
-          .join('-');
+    newNs = newNs.startsWith('global') ? newNs.split('-').splice(1).join('-') : newNs.split('-').splice(2).join('-');
+  }
+  /// #endif
+  return newNs;
+}
+
+export function reverseReduceNs(clusterId: string, namespace: string) {
+  let newNs = namespace;
+  /// #if project
+  //业务侧ns eg: cls-xxx-ns 需要去除前缀
+  if (newNs) {
+    newNs = `${clusterId}-${newNs}`;
   }
   /// #endif
   return newNs;

web/console/src/modules/alarmPolicy/WebAPI.ts
@@ -1,3 +1,4 @@
+import { reduceNs } from '@helper';
 import { OperationResult, QueryState, RecordSet, uuid } from '@tencent/ff-redux';
 
 import { resourceConfig } from '../../../config';
@@ -107,12 +108,7 @@ export async function fetchAlarmPolicy(query: QueryState<AlarmPolicyFilter>) {
   }
 
   // let response = await sendCapiRequest('tke', 'DescribeAlarmPolicies', params, query.filter.regionId);
-  let workloadTypeMap = {
-    Deployment: 'deployment',
-    StatefulSet: 'statefulset',
-    DaemonSet: 'daemonset',
-    TApp: 'tapp'
-  };
+
   alarmPolicyList = items.map(item => {
     let alarmPolicyMetricsConfig =
       (item.AlarmPolicySettings.AlarmPolicyType === 'cluster'
@@ -128,7 +124,7 @@ export async function fetchAlarmPolicy(query: QueryState<AlarmPolicyFilter>) {
       alarmPolicyType: item.AlarmPolicySettings.AlarmPolicyType,
       statisticsPeriod: item.AlarmPolicySettings.statisticsPeriod,
       alarmMetrics: [] as MetricsObject[],
-      alarmObjectWorkloadType: item.WorkloadType && workloadTypeMap[item.WorkloadType],
+      alarmObjectWorkloadType: item.WorkloadType,
       alarmObjectNamespace: item.Namespace,
       alarmObjetcsType: item.AlarmPolicySettings.AlarmObjectsType,
       alarmObjetcs: [],
@@ -186,12 +182,6 @@ export async function fetchAlarmPolicy(query: QueryState<AlarmPolicyFilter>) {
 }
 
 function getAlarmPolicyParams(alarmPolicyEdition: AlarmPolicyEdition[], opreator: AlarmPolicyOperator, receiverGroups) {
-  let workloadTypeMap = {
-    deployment: 'Deployment',
-    statefulset: 'StatefulSet',
-    daemonset: 'DaemonSet',
-    tapp: 'TApp'
-  };
   let params = {
     ClusterInstanceId: opreator.clusterId,
     AlarmPolicyId: alarmPolicyEdition[0].alarmPolicyId,
@@ -215,18 +205,20 @@ function getAlarmPolicyParams(alarmPolicyEdition: AlarmPolicyEdition[], opreator
       EnableShield: false
     }
   };
-  if (alarmPolicyEdition[0].alarmObjectsType === 'part') {
-    /// #if project
-    //业务侧的namespace需要做前缀处理
-    alarmPolicyEdition[0].alarmObjectNamespace = alarmPolicyEdition[0].alarmObjectNamespace
-      .split('-')
-      .splice(2)
-      .join('-');
-    /// #endif
-    params['Namespace'] = alarmPolicyEdition[0].alarmObjectNamespace;
-
-    params['WorkloadType'] = workloadTypeMap[alarmPolicyEdition[0].alarmObjectWorkloadType];
+  if (alarmPolicyEdition[0].alarmPolicyType === 'pod') {
+    if (alarmPolicyEdition[0].alarmObjectsType === 'all') {
+      if (alarmPolicyEdition[0].alarmObjectNamespace !== 'ALL') {
+        params['Namespace'] = reduceNs(alarmPolicyEdition[0].alarmObjectNamespace);
+      }
+      if (alarmPolicyEdition[0].alarmObjectWorkloadType !== 'ALL') {
+        params['WorkloadType'] = alarmPolicyEdition[0].alarmObjectWorkloadType;
+      }
+    } else {
+      params['Namespace'] = reduceNs(alarmPolicyEdition[0].alarmObjectNamespace);
+      params['WorkloadType'] = alarmPolicyEdition[0].alarmObjectWorkloadType;
+    }
   }
+
   alarmPolicyEdition[0].alarmMetrics.forEach(item => {
     if (item.enable) {
       let metrics = {

web/console/src/modules/alarmPolicy/actions/alarmPolicyActions.ts
@@ -11,6 +11,7 @@ import * as WebAPI from '../WebAPI';
 import { namespaceActions } from './namespaceActions';
 import { validatorActions } from './validatorActions';
 import { workloadActions } from './workloadActions';
+import { reverseReduceNs } from '@helper/urlUtil';
 
 type GetState = () => RootState;
 
@@ -22,10 +23,7 @@ const _alarmPolicyActions = createFFListActions<AlarmPolicy, AlarmPolicyFilter>(
     //业务侧中过滤只有这个namepace下的AlarmPolicy
     /// #if project
     let { namespaceSelection, route } = getstate();
-    let namespace = (getstate().namespaceSelection || route.queries['np'])
-      .split('-')
-      .splice(2)
-      .join('-');
+    let namespace = (getstate().namespaceSelection || route.queries['np']).split('-').splice(2).join('-');
     response.records = response.records.filter(
       item =>
         item.alarmObjetcsType === 'part' && item.alarmObjectNamespace === namespace && item.alarmPolicyType === 'pod'
@@ -69,6 +67,12 @@ const editActions = {
         type: ActionType.InputAlarmPolicyType,
         payload: alarmpolicy.alarmPolicyType
       });
+      if (alarmpolicy.alarmPolicyType === 'pod') {
+        dispatch({
+          type: ActionType.InputAlarmPolicyObjectsType,
+          payload: 'part'
+        });
+      }
 
       dispatch(alarmPolicyActions.initAlarmMetricsForUpdate(alarmpolicy.alarmMetrics, alarmpolicy.alarmPolicyType));
       dispatch(
@@ -95,22 +99,30 @@ const editActions = {
       if (alarmpolicy.alarmPolicyType !== 'cluster') {
         dispatch(alarmPolicyActions.inputAlarmPolicyObjectsType(alarmpolicy.alarmObjetcsType));
         //告警对象是workload且选择按工作负载选择初始化
-        if (alarmpolicy.alarmObjetcsType === 'part') {
+        if (alarmpolicy.alarmPolicyType === 'pod') {
           let namespace;
-          /// #if project
-          //业务侧的namespace需要做前缀处理
-          namespace = `${alarmpolicy.clusterId}-${alarmpolicy.alarmObjectNamespace}`;
-          /// #endif
-          /// #if tke
-          namespace = alarmpolicy.alarmObjectNamespace;
-          /// #endif
+          let workType;
+          if (alarmpolicy.alarmObjetcsType === 'part') {
+            namespace = reverseReduceNs(alarmpolicy.clusterId, alarmpolicy.alarmObjectNamespace);
+            workType = alarmpolicy.alarmObjectWorkloadType;
+            dispatch(alarmPolicyActions.inputAlarmPolicyObjects(alarmpolicy.alarmObjetcs));
+          } else {
+            if (alarmpolicy.alarmObjectNamespace) {
+              namespace = reverseReduceNs(alarmpolicy.clusterId, alarmpolicy.alarmObjectNamespace);
+            } else {
+              namespace = 'ALL';
+            }
+            if (alarmpolicy.alarmObjectWorkloadType) {
+              workType = alarmpolicy.alarmObjectWorkloadType;
+            } else {
+              workType = 'ALL';
+            }
+          }
           dispatch({
             type: ActionType.InputAlarmWorkLoadNameSpace,
             payload: namespace
           });
-
           dispatch(alarmPolicyActions.inputAlarmObjectWorkloadType(alarmpolicy.alarmObjectWorkloadType));
-          dispatch(alarmPolicyActions.inputAlarmPolicyObjects(alarmpolicy.alarmObjetcs));
         }
       }
     };
@@ -148,12 +160,14 @@ const editActions = {
         );
         //初始化workload列表不使用初始值
         /// #if tke
-        if (mode === 'update' && finder.alarmPolicyType === 'pod' && finder.alarmObjetcsType === 'part') {
-          namespaceActions.applyFilter({
-            regionId: route.queries['rid'],
-            clusterId: route.queries['clusterId'],
-            default: false
-          });
+        if (mode === 'update' && finder.alarmPolicyType === 'pod') {
+          dispatch(
+            namespaceActions.applyFilter({
+              regionId: route.queries['rid'],
+              clusterId: route.queries['clusterId'],
+              default: false
+            })
+          );
         } else {
           dispatch(
             namespaceActions.applyFilter({
@@ -249,10 +263,31 @@ const editActions = {
   //alarm选择告警对象类型
   inputAlarmPolicyObjectsType: objectType => {
     return (dispatch, getState: GetState) => {
+      let { alarmPolicyEdition, namespaceList } = getState();
       dispatch({
         type: ActionType.InputAlarmPolicyObjectsType,
         payload: objectType
       });
+      if (alarmPolicyEdition.alarmPolicyType === 'pod') {
+        if (objectType === 'part') {
+          if (namespaceList.data.recordCount > 0 && alarmPolicyEdition.alarmObjectNamespace === 'ALL') {
+            dispatch({
+              type: ActionType.InputAlarmWorkLoadNameSpace,
+              payload: namespaceList.data.records[0].name
+            });
+          }
+          dispatch(alarmPolicyActions.inputAlarmObjectWorkloadType('Deployment'));
+        } else {
+          dispatch({
+            type: ActionType.InputAlarmObjectWorkloadType,
+            payload: 'ALL'
+          });
+          dispatch({
+            type: ActionType.InputAlarmWorkLoadNameSpace,
+            payload: 'ALL'
+          });
+        }
+      }
     };
   },
 
@@ -465,14 +500,18 @@ const editActions = {
         type: ActionType.InputAlarmPolicyObjects,
         payload: []
       });
-      dispatch(
-        workloadActions.applyFilter({
-          regionId: +regionSelection.value,
-          clusterId: cluster.selection ? cluster.selection.metadata.name : '',
-          namespace: namespace,
-          workloadType: alarmPolicyEdition.alarmObjectWorkloadType
-        })
-      );
+      if (alarmPolicyEdition.alarmPolicyType === 'pod' && alarmPolicyEdition.alarmObjectsType === 'all') {
+        //
+      } else {
+        dispatch(
+          workloadActions.applyFilter({
+            regionId: +regionSelection.value,
+            clusterId: cluster.selection ? cluster.selection.metadata.name : '',
+            namespace: namespace,
+            workloadType: alarmPolicyEdition.alarmObjectWorkloadType
+          })
+        );
+      }
     };
   },
   inputAlarmObjectWorkloadType: (type: string) => {
@@ -486,14 +525,18 @@ const editActions = {
         type: ActionType.InputAlarmPolicyObjects,
         payload: []
       });
-      dispatch(
-        workloadActions.applyFilter({
-          regionId: +regionSelection.value,
-          clusterId: cluster.selection ? cluster.selection.metadata.name : '',
-          namespace: alarmPolicyEdition.alarmObjectNamespace,
-          workloadType: type
-        })
-      );
+      if (alarmPolicyEdition.alarmPolicyType === 'pod' && alarmPolicyEdition.alarmObjectsType === 'all') {
+        //
+      } else {
+        dispatch(
+          workloadActions.applyFilter({
+            regionId: +regionSelection.value,
+            clusterId: cluster.selection ? cluster.selection.metadata.name : '',
+            namespace: alarmPolicyEdition.alarmObjectNamespace,
+            workloadType: type
+          })
+        );
+      }
     };
   },
 

web/console/src/modules/alarmPolicy/actions/workloadActions.ts
@@ -15,7 +15,13 @@ const fetchWorkloadActions = generateFetcherActionCreator({
   fetcher: async (getState: GetState, fetchOptions, dispatch: Redux.Dispatch) => {
     let { workloadQuery, clusterVersion } = getState();
     let { filter } = workloadQuery;
-    let resourceInfo = resourceConfig(clusterVersion)[filter.workloadType];
+    let workloadTypeMap = {
+      Deployment: 'deployment',
+      StatefulSet: 'statefulset',
+      DaemonSet: 'daemonset',
+      TApp: 'tapp'
+    };
+    let resourceInfo = resourceConfig(clusterVersion)[workloadTypeMap[filter.workloadType]];
     let response = await WebAPI.fetchResourceList(getState().workloadQuery, { resourceInfo });
     return response;
   }

web/console/src/modules/alarmPolicy/components/EditAlarmPolicyObject.tsx
@@ -28,10 +28,6 @@ export class EditAlarmPolicyObject extends React.Component<RootProps, {}> {
       // 根据 PodList 初始化 checkedList
       let checkedList = [];
       let checkboxList = workloadList.data.records.map(workload => {
-        // let checkboxItem = {
-        //   value: workload.metadata.name,
-        //   label: workload.metadata.name
-        // };
         let checkbox = <Checkbox name={workload.metadata.name}>{workload.metadata.name}</Checkbox>;
         if (alarmPolicyEdition.alarmObjects.find(object => object === workload.metadata.name)) {
           checkedList.push(workload.metadata.name);
@@ -60,10 +56,25 @@ export class EditAlarmPolicyObject extends React.Component<RootProps, {}> {
       return <noscript />;
     }
     let finalWorkloadTypeList = workloadTypeList.slice();
+    let finalWorkloadNsList = namespaceList.data.records.map(ns => ({
+      value: ns.name,
+      label: ns.name
+    }));
     if (addons['TappController']) {
       finalWorkloadTypeList.push({
-        value: 'tapp',
+        value: 'TApp',
         label: 'TApp'
+      });
+    }
+
+    if (type === 'pod' && alarmPolicyEdition.alarmObjectsType === 'all') {
+      finalWorkloadTypeList.unshift({
+        value: 'ALL',
+        label: 'ALL'
+      });
+      finalWorkloadNsList.unshift({
+        value: 'ALL',
+        label: 'ALL'
       });
     }
     let radioList: JSX.Element[] = [];
@@ -77,63 +88,71 @@ export class EditAlarmPolicyObject extends React.Component<RootProps, {}> {
             </Radio>
           </div>
           <div className="alarm-write">
-            {item.value === 'part' && alarmPolicyEdition.alarmObjectsType === 'part' && (
-              <ul className="form-list fixed-layout">
-                <FormItem label="Namespace">
-                  <SelectList
-                    value={alarmPolicyEdition.alarmObjectNamespace + ''}
-                    recordList={namespaceList.data.records}
-                    valueField="name"
-                    textField="name"
-                    textFields={['name']}
-                    textFormat={`\${name}`}
-                    className="tc-15-select m"
-                    style={{ marginRight: '5px' }}
-                    onSelect={value => {
-                      actions.namespace.selectNamespace(value);
-                    }}
-                    isUnshiftDefaultItem={false}
-                  />
-                </FormItem>
-                <FormItem label="WorkloadType" isNeedFormInput={false}>
-                  <SelectList
-                    value={alarmPolicyEdition.alarmObjectWorkloadType + ''}
-                    recordList={finalWorkloadTypeList}
-                    valueField="value"
-                    textField="name"
-                    textFields={['label']}
-                    textFormat={`\${label}`}
-                    className="tc-15-select m"
-                    style={{ marginRight: '5px' }}
-                    onSelect={value => actions.alarmPolicy.inputAlarmObjectWorkloadType(value)}
-                    isUnshiftDefaultItem={false}
-                  />
-                  <div
-                    className="param-box"
-                    style={{
-                      backgroundColor: '#fff',
-                      padding: '5px 10px',
-                      marginTop: '5px',
-                      marginBottom: '10px',
-                      width: '250px'
-                    }}
-                  >
-                    <div className="param-bd">
-                      <ul>
-                        <div className="tc-g-u-1-3" style={{ width: '100%', maxHeight: '180px', overflowY: 'auto' }}>
-                          <div className="colony-list">{this.renderPodList()}</div>
+            {alarmPolicyEdition.alarmPolicyType === 'pod' &&
+              ((item.value === 'part' && alarmPolicyEdition.alarmObjectsType === 'part') ||
+                (item.value === 'all' && alarmPolicyEdition.alarmObjectsType === 'all')) && (
+                <ul className="form-list fixed-layout">
+                  <FormItem label="Namespace">
+                    <SelectList
+                      value={alarmPolicyEdition.alarmObjectNamespace + ''}
+                      recordList={finalWorkloadNsList}
+                      valueField="value"
+                      textFields={['label']}
+                      textFormat={`\${label}`}
+                      className="tc-15-select m"
+                      style={{ marginRight: '5px' }}
+                      onSelect={value => {
+                        actions.namespace.selectNamespace(value);
+                      }}
+                      isUnshiftDefaultItem={false}
+                    />
+                  </FormItem>
+                  <FormItem label="WorkloadType" isNeedFormInput={false}>
+                    <SelectList
+                      value={alarmPolicyEdition.alarmObjectWorkloadType + ''}
+                      recordList={finalWorkloadTypeList}
+                      valueField="value"
+                      textFields={['label']}
+                      textFormat={`\${label}`}
+                      className="tc-15-select m"
+                      style={{ marginRight: '5px' }}
+                      onSelect={value => actions.alarmPolicy.inputAlarmObjectWorkloadType(value)}
+                      isUnshiftDefaultItem={false}
+                    />
+                    {alarmPolicyEdition.alarmObjectsType === 'part' && (
+                      <>
+                        <div
+                          className="param-box"
+                          style={{
+                            backgroundColor: '#fff',
+                            padding: '5px 10px',
+                            marginTop: '5px',
+                            marginBottom: '10px',
+                            width: '250px'
+                          }}
+                        >
+                          <div className="param-bd">
+                            <ul>
+                              <div
+                                className="tc-g-u-1-3"
+                                style={{ width: '100%', maxHeight: '180px', overflowY: 'auto' }}
+                              >
+                                <div className="colony-list">{this.renderPodList()}</div>
+                              </div>
+                            </ul>
+                          </div>
                         </div>
-                      </ul>
-                    </div>
-                  </div>
-                  <div className="is-error">
-                    <p className="form-input-help" style={{ fontSize: '12px' }}>
-                      {alarmPolicyEdition.v_alarmObjects.status === 2 && alarmPolicyEdition.v_alarmObjects.message}
-                    </p>
-                  </div>
-                </FormItem>
-              </ul>
-            )}
+                        <div className="is-error">
+                          <p className="form-input-help" style={{ fontSize: '12px' }}>
+                            {alarmPolicyEdition.v_alarmObjects.status === 2 &&
+                              alarmPolicyEdition.v_alarmObjects.message}
+                          </p>
+                        </div>
+                      </>
+                    )}
+                  </FormItem>
+                </ul>
+              )}
           </div>
         </div>
       );

web/console/src/modules/alarmPolicy/constants/Config.ts
@@ -113,15 +113,15 @@ export const AlarmPolicyPhoneInterval = [
 
 export const workloadTypeList = [
   {
-    value: 'deployment',
+    value: 'Deployment',
     label: 'Deployment'
   },
   {
-    value: 'daemonset',
+    value: 'DaemonSet',
     label: 'DaemonSet'
   },
   {
-    value: 'statefulset',
+    value: 'StatefulSet',
     label: 'StatefulSet'
   }
 ];

web/console/src/modules/alarmPolicy/reducers/AlarmPolicyEditReducer.ts
@@ -40,7 +40,7 @@ const TempReducer = combineReducers({
 
   alarmObjectNamespace: reduceToPayload(ActionType.InputAlarmWorkLoadNameSpace, ''),
 
-  alarmObjectWorkloadType: reduceToPayload(ActionType.InputAlarmObjectWorkloadType, 'deployment'),
+  alarmObjectWorkloadType: reduceToPayload(ActionType.InputAlarmObjectWorkloadType, 'Deployment'),
 
   /**策略指标 */
   alarmMetrics: reduceToPayload(ActionType.InputAlarmMetrics, []),

commit 41ad0a9a44d119d2b42c90264dbc027873a73090

fix compatibility issue caused by commit eed3fda4518df0d3be481669f22ca76ae83833b9
Signed-off-by: Feng Kun <fengkun32@gmail.com>

web/console/helpers/urlUtil.ts
@@ -119,7 +119,6 @@ export const reduceK8sRestfulPath = (options: K8sRestfulPathOptions) => {
   let {
     resourceInfo,
     namespace = '',
-    middleKey = '',
     specificName = '',
     extraResource = '',
     clusterId = '',